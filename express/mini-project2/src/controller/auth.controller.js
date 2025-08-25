42529811be1349ed66f0e09a9ebcb7b49065bde7
arshiyagarg <arshiyagarg932@gmail.com>
2025-08-25 20:31:14 +0530

    mini-project : Task Manager

---
+++ express/mini-project2/src/controller/auth.controller.js
@@ -0,0 +1,23 @@
+export const login = (req, res) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).json({ error: "Username is required" });
+    }
+
+    req.session.user = { username };
+
+    res.cookie("username", username, { httpOnly: true, maxAge: 1000*60*60*24 }); // client side cookie 
+    res.json({ message: "Login successful", username });
+};
+
+export const logout = (req,res) => {
+    res.clearCookie("username");
+    req.session.destroy((err) => {
+        if(err){
+            return res.status(500).json({error:"Error logging out"})
+        }
+
+        res.json({message:"Logout successful"});
+    })
+}
---
+++ express/mini-project2/src/utils/data/tasks.js
@@ -0,0 +1,57 @@
+[
+  {
+    "id": 1,
+    "username": "Arshiya Garg",
+    "title": "newton",
+    "completed": false
+  },
+  {
+    "id": 1,
+    "username": "arshiya",
+    "title": "newton",
+    "desc": "3Laws",
+    "completed": false
+  },
+  {
+    "id": 1756115779168,
+    "username": "Arshiya",
+    "title": "newton",
+    "desc": "3 laws of newton",
+    "completed": false
+  },
+  {
+    "id": 1756115999208,
+    "username": "Arshiya",
+    "title": "temp-title",
+    "desc": "temp=desc",
+    "completed": false
+  },
+  {
+    "id": 1756117578853,
+    "username": "temp=data",
+    "title": "temp-title",
+    "desc": "temp-desc",
+    "completed": false
+  },
+  {
+    "id": 1756117629331,
+    "username": "temp=data",
+    "title": "temp-title",
+    "desc": "temp-desc",
+    "completed": false
+  },
+  {
+    "id": 32,
+    "username": "Avika",
+    "title": "ne",
+    "desc": "ne---desc",
+    "completed": false
+  },
+  {
+    "id": 5,
+    "username": "user",
+    "title": "user-title-2",
+    "desc": "user-desc-updated",
+    "completed": false
+  }
+]
