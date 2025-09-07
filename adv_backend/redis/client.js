c59d8d22acf46770078a03e124ac1e3c51524121
arshiyagarg <arshiyagarg932@gmail.com>
2025-09-07 20:30:17 +0530

    redis using placeholder api

--- adv_backend/redis/client.js
+++ adv_backend/redis/client.js
@@ -1,4 +1,7 @@
 const redis = require("ioredis");
-const client = new redis();
+const client = new redis({
+  host: "127.0.0.1", // Docker port mapped to localhost, UI 
+  port: 6379 // docker port 
+});
 
 module.exports = client;
